6b1b892e8f29d83c03b9c250e49997ebf4f6f844
rhclaudio <rhclaudio@gmail.com>
2019-10-09 15:57:02 -0300

    adding new module

--- testing/main.tf
+++ testing/main.tf
@@ -9,3 +9,13 @@ module "webserver" {
   subnet_id = "subnet-d7aa228f"
 }
 
+module "webserver2" {
+  source = "github.com/agucampao/terraform-module-ec2-with-eip.git"
+  ami = "ami-0c6b1d09930fac512"
+  instance_type = "t2.micro"
+  key_name = "mykey"
+  vpc_id = "vpc-9171a2eb"
+  project_name = "webserver"
+  environment = "testing"
+  subnet_id = "subnet-d7aa228f"
+}
